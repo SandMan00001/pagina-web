6ed7bda8dfefcb7ea8e6d09d51ab4526dbd85af0
gabriele saija <87981054+SandMan00001@users.noreply.github.com>
2026-05-27 21:09:06 +0200

    Update Router base name in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Contattaci } from './pages/Contattaci';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basenam="/pagina-web">
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
