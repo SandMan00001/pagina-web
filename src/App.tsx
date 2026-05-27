d5c30b088c165d53f49279fadd073fb5f110c39d
gabriele saija <87981054+SandMan00001@users.noreply.github.com>
2026-05-27 21:11:17 +0200

    Fix typo in Router basename attribute

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Contattaci } from './pages/Contattaci';
 
 const App: React.FC = () => {
   return (
-    <Router basenam="/pagina-web">
+    <Router basename="/pagina-web">
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
