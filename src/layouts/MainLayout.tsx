51b01c013dafec11cd34d61f451518c6b26ad57e
SandMan00001 <gabriele.saija.2003@gmail.com>
2026-06-03 11:11:42 +0200

    aggiuntga social

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -73,10 +73,10 @@ export const MainLayout: React.FC = () => {
           © 2026 FounDreams. Eccellenza tecnica e innovazione visionaria.
         </p>
         <div className="flex gap-md items-center">
-          <a href="#" className="text-on-surface-variant hover:text-secondary transition-all duration-300 transform hover:scale-110">
+          <a href="https://www.instagram.com/foundreams_?igsh=cnF6a3gzZXZxd3Rq" target="_blank" rel="noopener noreferrer" className="text-on-surface-variant hover:text-secondary transition-all duration-300 transform hover:scale-110">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-instagram w-6 h-6"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg>
           </a>
-          <a href="#" className="text-on-surface-variant hover:text-secondary transition-all duration-300 transform hover:scale-110">
+          <a href="https://www.facebook.com/people/FounDreams/61590709104730/" target="_blank" rel="noopener noreferrer" className="text-on-surface-variant hover:text-secondary transition-all duration-300 transform hover:scale-110">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-facebook w-6 h-6"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
           </a>
         </div>
