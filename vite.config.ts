31494fc0aee07157790cc3e0b47ea63d98c998e0
SandMan00001 <gabriele.saija.2003@gmail.com>
2026-06-04 11:06:02 +0200

    fix: update CSP to allow Google Analytics connect-src

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const injectCSP = () => {
     name: 'inject-csp',
     transformIndexHtml(html: string, ctx: any) {
       if (!ctx.server) { // Eseguito solo durante 'vite build'
-        const cspMeta = `<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self';">`;
+        const cspMeta = `<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://region1.google-analytics.com https://www.google-analytics.com https://analytics.google.com;">`;
         return html.replace('</head>', `  ${cspMeta}\n</head>`);
       }
       return html;
