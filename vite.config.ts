c24ecf1a584060bd01995c82a4c4a24765f31a06
SandMan00001 <gabriele.saija.2003@gmail.com>
2026-06-22 21:19:14 +0200

    messa apposto lingue sito

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const injectCSP = () => ({
     const cspMeta = `
 <meta
   http-equiv="Content-Security-Policy"
-  content="default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://cdn.iubenda.com https://*.iubenda.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.iubenda.com https://*.iubenda.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: https: https://cdn.iubenda.com https://*.iubenda.com; connect-src 'self' https://region1.google-analytics.com https://www.google-analytics.com https://analytics.google.com https://cdn.iubenda.com https://*.iubenda.com; frame-src 'self' https://www.iubenda.com https://embeds.iubenda.com https://*.iubenda.com;"
+  content="default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://cdn.iubenda.com https://*.iubenda.com https://translate.google.com https://translate.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.iubenda.com https://*.iubenda.com https://translate.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: https: https://cdn.iubenda.com https://*.iubenda.com https://translate.googleapis.com; connect-src 'self' https://region1.google-analytics.com https://www.google-analytics.com https://analytics.google.com https://cdn.iubenda.com https://*.iubenda.com https://translate.googleapis.com; frame-src 'self' https://www.iubenda.com https://embeds.iubenda.com https://*.iubenda.com;"
 />`.trim()
 
     return html.replace('</head>', `  ${cspMeta}\n</head>`)
